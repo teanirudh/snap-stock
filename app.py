b4e7aee1347672c618204759a9208d3658e9d5e9
teanirudh <teanirudh@gmail.com>
2023-11-26 14:17:39 +0530

    Add loggers

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+from logging import INFO, basicConfig, getLogger
+
 from flask import Flask, jsonify, make_response, render_template, request
 
 app = Flask(__name__)
 
+basicConfig(level=INFO)
+logger = getLogger(__name__)
+
 
 def get_count(image):
+    # TODO: Send image to the model and return count
     return 123
 
 
@@ -16,10 +22,13 @@ def index():
 def process():
     try:
         image = request.files["image"]
+        logger.info("Received request to process image - %s", image.filename)
         count = get_count(image)
+        logger.info("Successfully processed image - Count = %d", count)
         response = make_response(jsonify({"count": str("{:03d}".format(count))}), 200)
         return response
     except Exception as e:
+        logger.error("%s", e)
         response = make_response(jsonify({"error": "ERR"}), 500)
         return response
 
